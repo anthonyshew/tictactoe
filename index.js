3635fde537e34c00c44d76403f3db43ac5e7f237
Anthony Shew <anthonyshew@gmail.com>
2019-08-25 09:53:04 -0700

    Bug fix: Can now win on last move instead of always seeing result of game being draw.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const animationWinShakeDuration = 2000;
+
 let gameConfig = {
     gameOn: false,
     gameBoard: [null, null, null, null, null, null, null, null, null],
@@ -30,20 +32,22 @@ const matchReset = () => {
         oWins: 0,
         draws: 0
     };
-    gameTiles.map(elem => { elem.innerHTML = ""; elem.classList.remove("X", "O") });
+    gameTiles.map(elem => { elem.innerHTML = ""; elem.classList.remove("X", "O", "shake-win") });
 }
 
 const gameReset = () => {
-    gameConfig = {
-        gameOn: false,
-        gameBoard: [null, null, null, null, null, null, null, null, null],
-        isXTurn: null,
-        gameResult: false,
-        xWins: gameConfig.xWins,
-        oWins: gameConfig.oWins,
-        draws: gameConfig.draws
-    };
-    gameTiles.map(elem => { elem.innerHTML = ""; elem.classList.remove("X", "O") });
+    setTimeout(() => {
+        gameConfig = {
+            gameOn: false,
+            gameBoard: [null, null, null, null, null, null, null, null, null],
+            isXTurn: null,
+            gameResult: false,
+            xWins: gameConfig.xWins,
+            oWins: gameConfig.oWins,
+            draws: gameConfig.draws
+        };
+        gameTiles.map(elem => { elem.innerHTML = ""; elem.classList.remove("X", "O", "shake-win") });
+    }, animationWinShakeDuration)
 }
 
 const gameStart = () => {
@@ -59,26 +63,35 @@ const turnToggle = () => {
 }
 
 const xWins = () => {
+    gameConfig.gameOn = false;
     gameConfig.xWins = gameConfig.xWins + 1;
     scoreboardUpdate();
-    return gameReset();
+    gameReset();
 }
 
 const oWins = () => {
+    gameConfig.gameOn = false;
     gameConfig.oWins = gameConfig.oWins + 1;
     scoreboardUpdate();
-    return gameReset();
+    gameReset();
 }
 
 const draw = () => {
+    gameConfig.gameOn = false;
     gameConfig.draws = gameConfig.draws + 1;
     scoreboardUpdate();
-    return gameReset();
+    gameReset();
 }
 
-const turnTaker = (e) => {
-    // Check if game is started
-    if (gameConfig.gameOn === false) return console.log("Hey! We're not playing yet!");
+const animateWin = (mark1, mark2, mark3) => {
+    document.getElementById(mark1).classList.add("shake-win");
+    document.getElementById(mark2).classList.add("shake-win");
+    document.getElementById(mark3).classList.add("shake-win");
+}
+
+const takeTurn = (e) => {
+    // Check if game is on
+    if (gameConfig.gameOn === false) return console.log("Hey! We're not playing at the moment!");
     // Check if game tile is already marked
     if (e.target.innerHTML !== "") return console.log("Already marked, friend!");
     let gameBoardIndex = e.target.id;
@@ -88,23 +101,34 @@ const turnTaker = (e) => {
     document.getElementById(gameBoardIndex).classList.add(currentTurn);
     if (checkWinner() === "meow") {
         return draw();
-    } else { if (checkWinner()) { currentTurn === "X" ? xWins() : oWins(); } };
+    } else {
+        if (checkWinner()[0]) { animateWin(checkWinner()[1], checkWinner()[2], checkWinner()[3]); currentTurn === "X" ? xWins() : oWins() };
+    }
     turnToggle();
 }
 
 const checkWinner = () => {
     let gameBoard = gameConfig.gameBoard;
-    // This can definitely be written as a nice function
-    if (gameBoard[0] !== null && gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) { console.log("Winner, across the top."); return true; };
-    if (gameBoard[3] !== null && gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) { console.log("Winner, across the middle."); return true; };
-    if (gameBoard[6] !== null && gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) { console.log("Winner, across the bottom."); return true; };
-    if (gameBoard[0] !== null && gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) { console.log("Winner, column one."); return true; };
-    if (gameBoard[1] !== null && gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) { console.log("Winner, column two."); return true; };
-    if (gameBoard[2] !== null && gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) { console.log("Winner, column three."); return true; };
-    if (gameBoard[0] !== null && gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) { console.log("Winner, diagonal left to right."); return true; };
-    if (gameBoard[2] !== null && gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) { console.log("Winner, diagonal right to left."); return true; };
-    if (gameBoard.every(elem => elem !== null)) { console.log("Kitty cats game!"); return "meow"; };
-    return false;
+
+    const winTester = (mark1, mark2, mark3, message) => {
+        if (gameBoard[mark1] !== null && gameBoard[mark1] === gameBoard[mark2] && gameBoard[mark1] === gameBoard[mark3]) { console.log(message); return [true, mark1, mark2, mark3] }
+        else { return null };
+    };
+
+    let winningCombination = [winTester(0, 1, 2, "Winner, across the top."),
+    winTester(3, 4, 5, "Winner, across the middle."),
+    winTester(6, 7, 8, "Winner, across the bottom."),
+    winTester(0, 3, 6, "Winner, column one."),
+    winTester(1, 4, 7, "Winner, column two."),
+    winTester(2, 5, 8, "Winner, column three."),
+    winTester(0, 4, 8, "Winner, diagonal left to right."),
+    winTester(2, 4, 6, "Winner, diagonal right to left.")
+    ].filter(elem => elem !== null)[0];
+    if (winningCombination === undefined) { winningCombination = [null] };
+    if (gameBoard.every(elem => elem !== null) && winningCombination.length === 1) { console.log("Kitty cats game!"); winningCombination = "meow"; };
+    console.log(gameBoard);
+    console.log(winningCombination);
+    return winningCombination;
 }
 
 const getViewportDimensions = () => {
@@ -116,7 +140,7 @@ const getViewportDimensions = () => {
 }
 
 // Listeners listening
-gameTiles.map(elem => elem.addEventListener("click", turnTaker));
+gameTiles.map(elem => elem.addEventListener("click", takeTurn));
 document.getElementById("game-start").addEventListener('click', gameStart);
 document.getElementById("match-reset").addEventListener('click', matchReset);
 document.getElementById("x-wins").addEventListener('click', xWins);
